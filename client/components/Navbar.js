0a27bfd03cc4b45419dbb72cee177aa82e389c14
Fiora De Marco <fiorademarco7@gmail.com>
2022-03-01 23:39:09 -0500

    Got data from single project props

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { logout } from '../store'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { logout } from "../store";
 
 const Navbar = () => {
-  const isLoggedIn = useSelector((state) => !!state.auth.id)
-  const dispatch = useDispatch()
+  const isLoggedIn = useSelector((state) => !!state.auth.id);
+  const dispatch = useDispatch();
 
   return (
     <div>
@@ -31,7 +31,7 @@ const Navbar = () => {
       </nav>
       <hr />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/components/Products.js
+++ client/components/Products.js
@@ -1,48 +1,49 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import Product from "./Product";
 
 const Products = () => {
   // const dispatch = useDispatch()
-  const [products, setProducts] = useState([])
-  // const products = [
-  //   {
-  //     id: 1,
-  //     title: 'Beach Bum',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Bikini Bottom',
-  //   },
-  //   {
-  //     id: 3,
-  //     title: 'Blackberry Smash',
-  //   },
-  //   {
-  //     id: 4,
-  //     title: 'Blood Orange Martini',
-  //   },
-  //   {
-  //     id: 5,
-  //     title: 'Blood Orange Tea',
-  //   },
-  //   {
-  //     id: 6,
-  //     title: 'Blue Velvet',
-  //   },
-  //   {
-  //     id: 7,
-  //     title: 'Body Electric',
-  //   },
-  //   {
-  //     id: 8,
-  //     title: 'Born to Die',
-  //   },
-  // ]
+  //const [products, setProducts] = useState([]);
+  const products = [
+    {
+      id: 1,
+      title: "Beach Bum",
+    },
+    {
+      id: 2,
+      title: "Bikini Bottom",
+    },
+    {
+      id: 3,
+      title: "Blackberry Smash",
+    },
+    {
+      id: 4,
+      title: "Blood Orange Martini",
+    },
+    {
+      id: 5,
+      title: "Blood Orange Tea",
+    },
+    {
+      id: 6,
+      title: "Blue Velvet",
+    },
+    {
+      id: 7,
+      title: "Body Electric",
+    },
+    {
+      id: 8,
+      title: "Born to Die",
+    },
+  ];
 
   // useEffect(() => {
-  //   setProducts(useSelector((state) => state.products))
-  // }, [])
+  //   setProducts(useSelector((state) => state.products));
+  // }, []);
 
   return !products.length ? (
     <div>Hellooo</div>
@@ -50,12 +51,14 @@ const Products = () => {
     <div>
       <div>This is going to be if we have data</div>
       {products.map((product) => (
-        <div key={product.id} {...product}>
-          <Link to={`/products/${product.id}`}>Product</Link>
+        <div key={product.id}>
+          <Link to={`/products/${product.id}`}>
+            <Product props={product} />
+          </Link>
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
