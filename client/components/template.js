89bd2938d81e6759035cf82ae111fcbd4b8a7aa8
JollyRen <jollyren@gmail.com>
2022-03-03 23:19:25 +0100

    added a functional component template

---
+++ client/components/template.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+function ComponentName(props) {
+  // const dispatch = useDispatch()
+  // const variables = useSelector((state) => state.variable)
+  // useEffect(() => {
+  //   async functionName() {
+  //      //enter logic here
+  //   }
+  //   functionName()
+  // }, [])
+
+  return (
+    <div>
+      <div>Hello</div>
+    </div>
+  )
+}
+
+export default ComponentName
