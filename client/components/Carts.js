bb4fd94260bff34faf93116a2908540af2a5685f
Fiora De Marco <fiorademarco7@gmail.com>
2022-03-07 23:37:23 -0500

    got order and cart

--- client/components/Carts.js
+++ client/components/Carts.js
@@ -3,17 +3,27 @@ import { useSelector, useDispatch } from "react-redux";
 import Product from "./Product";
 import { fetchProducts } from "../store/";
 import { Link } from "react-router-dom";
-import { fetchOrder, getOrder } from "../store/order";
+import { fetchOrder, getOrder, getCart, fetchCart } from "../store/order";
 
 const Carts = () => {
   const dispatch = useDispatch();
   const orders = useSelector((state) => state.orders);
   const { id } = useSelector((state) => state.auth);
+  const carts = useSelector((state) => state.carts);
 
   useEffect(() => {
     dispatch(fetchOrder(id));
   });
-  return <div>{orders}</div>;
+
+  useEffect(() => {
+    dispatch(fetchCart(id));
+  });
+  return (
+    <div>
+      <div>{orders}</div>
+      <div>{carts}</div>
+    </div>
+  );
 };
 
 export default Carts;
--- client/store/order.js
+++ client/store/order.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 export const GET_ORDER = "GET_ORDER";
 
+export const GET_CART = "GET_CART";
+
 export const getOrder = (order) => {
   return {
     type: GET_ORDER,
@@ -8,14 +10,34 @@ export const getOrder = (order) => {
   };
 };
 
-export const fetchOrder = (id) => {
+export const getCart = (cart) => {
+  return {
+    type: GET_CART,
+    cart,
+  };
+};
+
+export const fetchOrder = (userId) => {
   return async (dispatch) => {
     try {
-      const response = await axios.post(`/api/orders/findOrCreateOrder/${id}`);
+      const response = await axios.post(
+        `/api/orders/findOrCreateOrder/${userId}`
+      );
       const data = response.data;
       dispatch(getOrder(data));
     } catch (error) {
       console.log(error);
+    }
+  };
+};
+
+export const fetchCart = (userId) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`/api/orders/details/${userId}`);
+      dispatch(getCart(data.orders[0].products));
+    } catch (error) {
+      next(error);
     }
   };
 };
@@ -25,6 +47,8 @@ export default function (state = initialState, action) {
   switch (action.type) {
     case GET_ORDER:
       return action.order;
+    case GET_CART:
+      return action.cart;
     default:
       return state;
   }
