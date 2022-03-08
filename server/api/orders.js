2b9995c6409e13fd60aadb683da1270905c68b1d
Hannah Gassmann <94023194+hgassmann91@users.noreply.github.com>
2022-03-08 16:55:00 -0600

    corrected order bug after auth

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,21 +1,21 @@
-const router = require('express').Router()
-const Cart = require('../db/models/Cart')
-const Product = require('../db/models/Product')
-const User = require('../db/models/User')
-const Order = require('../db/models/Order')
-const { requireToken, isAdmin } = require('./gatekeepingMiddleware')
+const router = require('express').Router();
+const Cart = require('../db/models/Cart');
+const Product = require('../db/models/Product');
+const User = require('../db/models/User');
+const Order = require('../db/models/Order');
+const { requireToken, isAdmin } = require('./gatekeepingMiddleware');
 
 //PRODUCTS GET ROUTER
 
 // /api/orders
 router.get('/', requireToken, isAdmin, async (req, res, next) => {
   try {
-    const orders = await Order.findAll()
-    res.send(orders)
+    const orders = await Order.findAll();
+    res.send(orders);
   } catch (error) {
-    console.error('No orders found')
+    console.error('No orders found');
   }
-})
+});
 
 //What compare req.user.id too. Compare with id from order?
 router.put('/purchase', requireToken, async (req, res, next) => {
@@ -24,50 +24,55 @@ router.put('/purchase', requireToken, async (req, res, next) => {
       where: {
         isPurchased: false,
       },
-    })
-    res.send(await order.update({ ...order, isPurchased: true }))
+    });
+    res.send(await order.update({ ...order, isPurchased: true }));
   } catch (error) {
-    console.error('No orders found')
+    console.error('No orders found');
   }
-})
+});
 
 // /api/orders/:id
 router.get('/details/:id', requireToken, async (req, res, next) => {
   try {
-    if (req.user.id === req.params.id) {
-      const userId = req.params.id
-      let userOrderDetails = await User.findOne({
-        where: { id: userId },
+    const userId = req.params.id;
+    let userOrderDetails = await User.findOne({
+      where: { id: userId },
+      include: {
+        model: Order,
+        where: { isPurchased: false },
         include: {
-          model: Order,
-          where: { isPurchased: false },
-          include: {
-            model: Product,
-          },
+          model: Product,
         },
-      })
-      res.json(userOrderDetails)
-    }
+      },
+    });
+    res.json(userOrderDetails);
   } catch (error) {
-    console.error('No order details found')
+    console.error('No order details found');
   }
-})
+});
 
-router.get('/findOrCreateOrder/:userId', requireToken, async (req, res, next) => {
-  try {
-    if (req.user.id === req.params.userid) {
-      const userId = req.params.userId
+router.get(
+  '/findOrCreateOrder/:userId',
+  requireToken,
+  async (req, res, next) => {
+    try {
+      const userId = req.user.id;
       const [order, created] = await Order.findOrCreate({
         where: {
           userId,
           isPurchased: false,
         },
-      })
-      res.status(201).json(order)
-    }
-  } catch (error) {
-    console.error('No Order found')
-  }
-})
+      });
 
-module.exports = router
+      if (created) {
+        res.status(201).json(await Order.create(order));
+      }
+
+      res.status(201).json(order);
+    } catch (error) {
+      console.error('No Order found');
+    }
+  }
+);
+
+module.exports = router;
--- server/api/users.js
+++ server/api/users.js
@@ -1,54 +1,54 @@
-const router = require('express').Router()
+const router = require('express').Router();
 const {
   models: { User, Product },
-} = require('../db')
-module.exports = router
-const { requireToken, isAdmin} = require('./gatekeepingMiddleware')
+} = require('../db');
+module.exports = router;
+const { requireToken, isAdmin } = require('./gatekeepingMiddleware');
 
 router.get('/', requireToken, isAdmin, async (req, res, next) => {
   try {
     const users = await User.findAll({
       attributes: ['id', 'username', 'role'],
-    })
+    });
     if (!users) {
-      res.status(404).send('No Users')
+      res.status(404).send('No Users');
     }
-    res.status(200).json(users)
+    res.status(200).json(users);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 // api/users/id (preferences and junk)
 router.get('/:id', requireToken, async (req, res, next) => {
   try {
-    if(req.user.id === req.params.id) {
+    if (req.user.id) {
       const user = await User.findOne({
         where: { id: req.params.id },
         include: Product,
-      })
-      res.json(user)
+      });
+      if (!user) {
+        res.status(404).send('Cannot find User');
+      }
+      res.json(user);
     } else {
-      res.status(403).send('This is not you!')
-    }
-    if (!user) {
-      res.status(404).send('Cannot find User')
+      res.status(403).send('This is not you!');
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 //create a new user
 //api/users/create
 router.post('/create', async (req, res, next) => {
   try {
-    const user = await User.create({ ...req.body, role: 'Customer' })
-    res.status(201).send(user)
+    const user = await User.create({ ...req.body, role: 'Customer' });
+    res.status(201).send(user);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 // o: you actually don't need to pass in the user id here since you can grab
 //  from req.user
@@ -59,16 +59,16 @@ router.post('/create', async (req, res, next) => {
 router.get('/:id/cart', requireToken, async (req, res, next) => {
   try {
     // o: make sure to check for the case when resource is not found
-    if(req.user.id === req.params.id) {
+    if (req.user.id === req.params.id) {
       const user = await User.findOne({
         where: {
           userId: req.user.id,
         },
-      })
-      const userCart = await user.getProducts()
-      res.json(userCart)
+      });
+      const userCart = await user.getProducts();
+      res.json(userCart);
     }
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
